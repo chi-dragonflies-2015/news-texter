1b0d5b37514b829654538d319662efb78b0c63da
Lola Adekunle <lola.adekunle@gmail.com>
2015-06-25 20:24:20 -0500

    Update css

--- public/js/application.js
+++ public/js/application.js
@@ -1,29 +1,27 @@
 $(document).ready(function() {
-  $('.container').on('click', '.login', function(event){
+  $('.big-text').on('click', '.login', function(event){
     event.preventDefault();
      var url = $(this).attr('href');
-     var $element = $(this);
      var request = $.ajax({ url: url,
                               method: "GET"
       });
         request.done(function(response){
-          $element.hide();
-          $('.signup').show();
-          $('.container').append(response);
+          $('li').removeClass('active');
+          $('this').parent().addClass('active');
+          $('.section').css('margin-left', '30px')
+          $('.section').html(response);
         });
     });
 
-     $('.container').on('click', '.signup', function(event){
+     $('.big-text').on('click', '.signup', function(event){
     event.preventDefault();
      var url = $(this).attr('href');
-     var $element = $(this);
      var request = $.ajax({ url: url,
                               method: "GET"
       });
         request.done(function(response){
-          $element.hide();
-          $('.login').show();
-          $('.container').append(response);
+          $('.section').css('margin-left', '120px');
+          $('.section').html(response);
         });
     });
 });
